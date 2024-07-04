a0cabc0cba6ae7d1b0709d3a2f1ee802646ff843
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2024-07-04 11:01:09 -0700

    Specify desired DSL for building models

---
+++ src/commonMain/kotlin/NewAddressBookWithGeneratedClasses.kt
@@ -0,0 +1,37 @@
+//fun newAddressBookWithGeneratedClasses(): MutableAddressBook = mutableAddressBook {
+//    root {
+//        name = "Address Book"
+//        lastUpdated = 1587147731UL
+//        settings {
+//            lastNameFirst = true
+//            encryptHeroName = false
+//            backgroundColor = "white"
+//            advanced {
+//                borderColor = "violet"
+//            }
+//        }
+//        groups {
+//            group {
+//                name = "DC"
+//                subGroups {
+//                    group {
+//                        name = "Superman"
+//                    }
+//                }
+//            }
+//            group {
+//                name = "Marvel"
+//            }
+//        }
+//        person {
+//            addressBookPerson {}
+//            addressBookPerson {}
+//        }
+//        cityInfo {
+//            addressBookCityInfo {}
+//            addressBookCityInfo {}
+//            addressBookCityInfo {}
+//            addressBookCityInfo {}
+//        }
+//    }
+//}
