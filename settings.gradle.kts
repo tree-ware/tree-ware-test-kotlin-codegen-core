b94218c3a1aafb56e36755fc0297bc2931da1935
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2024-12-29 22:14:11 -0800

    Test version 0.3.1.0 of the plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ pluginManagement {
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            val treeWareKotlinCoreVersion = version("treeWareKotlinCoreVersion", "0.3.0.0")
+            val treeWareKotlinCoreVersion = version("treeWareKotlinCoreVersion", "0.3.0.2")
             library("treeWareKotlinCore", "org.tree-ware.tree-ware-kotlin-core", "core").versionRef(
                 treeWareKotlinCoreVersion
             )
