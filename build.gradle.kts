b6189d249ce95e265347376c685b0e9b8dd58b64
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2025-07-23 16:40:23 -0700

    Upgrade Gradle, Kotlin, tree-ware core and plugin
    * Upgrade Gradle to 8.6
    * Upgrade Kotlin to 2.1.10
    * Upgrade tree-ware-kotlin-core to 0.5.0.0
    * Upgrade tree-ware core plugin to 0.5.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "org.tree-ware.tree-ware-test-kotlin-codegen-core"
-version = "0.4.0.0"
+version = "0.5.0.0"
 
 plugins {
-    kotlin("multiplatform") version "1.7.0"
+    kotlin("multiplatform") version "2.1.10"
     id("idea")
-    id("org.tree-ware.core") version "0.4.0.0"
+    id("org.tree-ware.core") version "0.5.0.0"
     id("com.dorongold.task-tree") version "2.1.1" // for listing task dependencies
 }
 
@@ -15,27 +15,20 @@ repositories {
     maven { url = uri("https://jitpack.io") }
 }
 
-tasks.withType<KotlinCompile> {
-    // Compile for Java 8 (default is Java 6)
-    kotlinOptions.jvmTarget = "1.8"
-}
-
 kotlin {
     jvm {
-        withJava()
     }
+
+    applyDefaultHierarchyTemplate()
+
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(libs.treeWareKotlinCore)
-                implementation(kotlin("stdlib"))
-            }
+        commonMain.dependencies {
+            implementation(libs.treeWareKotlinCore)
+            implementation(kotlin("stdlib"))
         }
-        val commonTest by getting {
-            dependencies {
-                implementation(libs.treeWareKotlinCoreTestFixtures)
-                implementation(kotlin("test"))
-            }
+        commonTest.dependencies {
+            implementation(libs.treeWareKotlinCoreTestFixtures)
+            implementation(kotlin("test"))
         }
     }
 }
