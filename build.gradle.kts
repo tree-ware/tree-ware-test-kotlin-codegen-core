3abde075a738454cf4191c1c2f1b06cffc3826db
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2025-09-09 11:10:23 -0700

    Upgrade tree-ware plugin to get the root "generate" task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "org.tree-ware.tree-ware-test-kotlin-codegen-core"
-version = "0.5.0.0"
+version = "0.5.1.0"
 
 plugins {
     kotlin("multiplatform") version "2.1.10"
     id("idea")
-    id("org.tree-ware.core") version "0.5.0.0"
+    id("org.tree-ware.core") version "0.5.1.0"
     id("com.dorongold.task-tree") version "2.1.1" // for listing task dependencies
 }
 
