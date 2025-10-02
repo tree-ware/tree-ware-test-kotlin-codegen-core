30a1d7c633cb08f006fda23ab503ffeac28ec2b7
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2025-10-01 21:34:08 -0700

    Test support for meta-model aux in generated code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,24 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "org.tree-ware.tree-ware-test-kotlin-codegen-core"
-version = "0.5.1.0"
+version = "0.5.2.0"
 
 plugins {
     kotlin("multiplatform") version "2.1.10"
     id("idea")
-    id("org.tree-ware.core") version "0.5.1.0"
+    id("org.tree-ware.core") version "0.5.2.0"
     id("com.dorongold.task-tree") version "2.1.1" // for listing task dependencies
 }
 
 repositories {
     mavenCentral()
     maven { url = uri("https://jitpack.io") }
+}
+
+treeWare {
+    metaModelAux {
+        auxClasses.add("org.treeWare.model.operator.set.aux.SetAuxPlugin")
+    }
 }
 
 kotlin {
